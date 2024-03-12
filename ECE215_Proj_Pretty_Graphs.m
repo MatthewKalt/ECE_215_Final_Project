322408a297b3e00da077d9ed2e5986a0c6706550
MatthewKalt <mkaltman@ucsc.edu>
2024-03-11 21:28:43 -0700

    changed graphs

--- ECE215_Proj_Pretty_Graphs.m
+++ ECE215_Proj_Pretty_Graphs.m
@@ -2001,13 +2001,13 @@ DesiredPos = [[0.0066 0.0053 0.8173],
 k = linspace(1,979,979);
 k = k*.035;
 figure(1);
-plot(k,ActualPos(:,1))
+plot(k,ActualPos(:,1));
 hold on
-plot(k,DesiredPos(:,1))
-plot(k,ActualPos(:,2))
-plot(k,DesiredPos(:,2))
-plot(k,ActualPos(:,3))
-plot(k,DesiredPos(:,3))
+plot(k,DesiredPos(:,1));
+plot(k,ActualPos(:,2));
+plot(k,DesiredPos(:,2));
+plot(k,ActualPos(:,3));
+plot(k,DesiredPos(:,3));
 legend('Actual_X','Desired_X','Actual_Y','Desired_Y','Actual_Z','Desired_Z');
 xlabel('Time [s]');
 ylabel('Position [M]');
